f5130f5a7fea42a2295889f69884a9eacf9d2c26
Edward <puffcorn1213@gmail.com>
2023-05-31 21:08:24 -0700

    testing321

--- db/users.js
+++ db/users.js
@@ -1,7 +1,7 @@
 const client = require("./client");
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 const SALT_COUNT = 5;
-const SALT_COUNT = 10; //test
+const SALT_COUNT = SALT_COUNT; //test
 // database functions
 
 // user functions
@@ -9,33 +9,34 @@ async function createUser({ username, password }) {
   const hashedPassword = bcrypt.hash(password, SALT_COUNT);
 
   try {
-    const {row: [user]} = await client.query(`
+    const {
+      row: [user],
+    } = await client.query(
+      `
     INSERT INTO users(username, password)
     VALUES ($1,$2)
     ON CONFLICT (username) DO NOTHING
     RETURNING id ,username
-    ;`,[username, hashedpassword]);
+    ;`,
+      [username, hashedPassword]
+    );
     return user;
-   } catch(error){
+  } catch (error) {
     throw error;
-   }
+  }
 }
-//userscommit
+
 async function getUser({ username, password }) {
-
+  console.log("hi");
 }
 
-async function getUserById(userId) {
+async function getUserById(userId) {}
 
-}
-
-async function getUserByUsername(userName) {
-
-}
+async function getUserByUsername(userName) {}
 
 module.exports = {
   createUser,
   getUser,
   getUserById,
   getUserByUsername,
-}
+};
