2acff4c83c3c6203c89f37883ba7d0e7713c551f
MtKrc <metekaraca@msn.com>
2023-06-12 18:39:39 -0700

    api_routineActivities_byMete

--- api/routineActivities.js
+++ api/routineActivities.js
@@ -1,44 +1,54 @@
 const express = require('express');
 const router = express.Router();
+const {
+    getRoutineActivityById,
+    getRoutineById,
+    updateRoutineActivity,
+    destroyRoutineActivity,
+  } = require("../db");
 
 // PATCH /api/routine_activities/:routineActivityId
-/*
-router.patch('/:routineActivityId', requireLogin, async (req, res, next) => {
-    const { } = req.body;
-
+router.patch('/:routineActivityId', async (req, res, next) => {
+    const { count, duration } = req.body;
+    const id = req.params.routineActivityId;
     try {
-        if () {
-            res.send({});
+      const routineActivity = await getRoutineActivityById(id);
+      const routine = await getRoutineById(routineActivity.routineId);
+      if (req.user.id !== routine.creatorId) {
+        next({ name: "Must be a user" });
+      } else {
+        const updatedRoutineAct = await updateRoutineActivity({
+          id,
+          count,
+          duration,
+        });
+        if (updatedRoutineAct) {
+          res.send(updatedRoutineAct);
         } else {
-            next({
-                name: "",
-                message: ''
-            })
+          next({ name: "Routine does not exist" });
         }
+      }
     } catch (error) {
-        next(error);
+      next(error);
     }
-})
-*/
+  }
+);
 
 // DELETE /api/routine_activities/:routineActivityId
-/*
-router.delete('/:routineActivityId', requireLogin, async (req, res, next) => {
-    const { } = req.body;
-
-    try {
-        if () {
-            res.send({});
+router.delete('/:routineActivityId', async (req, res, next) => {
+      const { routineActivityId } = req.params;
+      try {
+        const routineActivity = await getRoutineActivityById(routineActivityId);
+        const routine = await getRoutineById(routineActivity.routineId);
+        if (req.user.id === routine.creatorId) {
+          const destroyActivity = await destroyRoutineActivity(routineActivityId);
+          res.send(destroyActivity);
         } else {
-            next({
-                name: "",
-                message: ''
-            })
+          next({ message: "Error: Only the creator can delete a routine"});
         }
-    } catch (error) {
-        next(error);
+      } catch ({ message }) {
+        next({ message });
+      }
     }
-})
-*/
-
+  )
 module.exports = router;
