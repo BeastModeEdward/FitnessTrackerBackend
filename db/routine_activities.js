103ae72ba6aeda66dc9dc0d297dfdac3162210fe
MtKrc <metekaraca@msn.com>
2023-06-12 18:42:58 -0700

    db_routine_activities_replaceWithOldCodes

--- db/routine_activities.js
+++ db/routine_activities.js
@@ -10,7 +10,6 @@ async function addActivityToRoutine({
     const {rows: [routineActivity]} = await client.query(`
     INSERT INTO routine_activities("routineId", "activityId", count, duration)
     VALUES ($1, $2, $3, $4)
-    ON CONFLICT ("routineId", "activityId") DO NOTHING
     RETURNING *;
     `,[routineId, activityId, count, duration])
     return routineActivity;
@@ -29,32 +28,20 @@ async function getRoutineActivityById(id) {
   return routineActivity;
 } catch(error){
   console.error(error)
-  throw error;
 }
 }
 
 async function getRoutineActivitiesByRoutine({ id }) {
-//   try{
-//     const {rows: routineActivities} = await client.query(`
-//     SELECT *
-//     FROM routine_activities
-//     WHERE id = ${id};
-//     `)
-//     return routineActivities;
-//   } catch(error){
-//     console.error(error)
-//   }
-// }
-try {
-  const { rows: routine_activity } = await client.query(`
-    SELECT * FROM routine_activities
-    WHERE "routineId"=$1;
-  `, [id]);
-  return routine_activity;
-} catch (error) {
-  console.log("Error getting routine activities by routine")
-  throw error;
-}
+  try{
+    const {rows: routineActivities} = await client.query(`
+    SELECT *
+    FROM routine_activities
+    WHERE id = ${id};
+    `)
+    return routineActivities;
+  } catch(error){
+    console.error(error)
+  }
 }
 
 async function updateRoutineActivity({ id, ...fields }) {
@@ -71,57 +58,30 @@ try{ const{rows:[routineActivity]} = await client.query(`
 return routineActivity;
 }catch(error){
   console.error(error)
-  throw error;
 }
 }
 
 async function destroyRoutineActivity(id) {
-//   try{
-//     const {rows:[routineActivity] } = await client.query(`
-//     DELETE FROM routine_activities
-//     WHERE id = ${id}
-//     RETURNING *;
-//     `)
-//     return routineActivity;
-//   } catch(error){
-//     console.error(error)
-//   }
-// }
-try {
-  const { rows: [ routine_activity ]} = await client.query(`
-  DELETE FROM routine_activities
-  WHERE id = $1
-  RETURNING *
-  `, [id]);
-
-  return routine_activity;
-} catch (error) {
-  console.log("Error destroying routine activity")
-  throw error;
-}
+  try{
+    const {rows:[routineActivity] } = await client.query(`
+    DELETE FROM routine_activities
+    WHERE id = ${id}
+    RETURNING *;
+    `)
+    return routineActivity;
+  } catch(error){
+    console.error(error)
+  }
 }
 
 async function canEditRoutineActivity(routineActivityId, userId) {
-//   if(routineActivityId === userId){
-//     return true;
-//   } else{
-//     return false;
-//   }
-// }
-try {
-  const { rows: [routine_activities] } = await client.query(`
-    SELECT *
-    FROM routine_activities
-    JOIN routines ON routine_activities."routineId" = routines.id
-    WHERE "creatorId" = ${userId}
-    AND routine_activities.id = ${routineActivityId};
-    `, );
-    return routine_activities
-  } catch (error){
-    console.log(error)
-    throw error
+  if(routineActivityId === userId){
+    return true;
+  } else{
+    return false;
   }
 }
+
 
 module.exports = {
   getRoutineActivityById,
